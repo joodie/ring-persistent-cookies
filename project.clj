8382a885cbf92257c10d764e83be92d4dae16ab2
Joost Diepenmaat <joost@zeekat.nl>
2011-03-12 14:39:22 +0100

    use some "stable" version of clj-time to depend on

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-core "0.3.1"]
-                 [clj-time "0.3.0-SNAPSHOT"]]
+                 [org.clojars.retiman/clj-time "0.3.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
